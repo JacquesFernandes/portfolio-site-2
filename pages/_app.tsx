985d3bb9b9a7257c28971ba618fc478509c34a43
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2022-04-05 23:42:30 +0100

    Fix fontawesome icon rendering bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// SOURCE: https://stackoverflow.com/a/59429852
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+import '../styles/globals.css';
+
+// Prevent fontawesome from adding its CSS since we did it manually above:
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
