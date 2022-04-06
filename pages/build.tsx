0b9697b73e642764957698e4d8705498ca5cc1c9
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2022-04-06 23:14:37 +0100

    Create build page

---
+++ pages/build.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+import {PageLayout} from "../components/page-layout";
+
+
+
+const Build: NextPage = () => {
+	return(
+		<PageLayout title="Build" description="So, what you looking for?" >
+		</PageLayout>
+	);
+}
+
+export default Build;
