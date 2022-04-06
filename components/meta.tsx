8ad04732b08129c0d11b3d902a4e6b9100c7ca79
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2022-04-06 22:56:46 +0100

    Create Meta component

---
+++ components/meta.tsx
@@ -0,0 +1,26 @@
+import {FC} from "react";
+
+interface MetaProps {
+	title: string;
+	description?: string;
+	keywords?: string[];
+}
+
+export const Meta: FC<MetaProps> = (props) => {
+  return(
+		<>
+			<meta name="title" content={`${props.title} - Jacques Fernandes`} />
+			<meta name="description" content={props.description} />
+			<meta name="keywords" content={props.keywords!.join(', ')} />
+			<meta name="robots" content="index, follow" />
+			<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+			<meta name="language" content="English" />
+			<meta name="author" content="Jacques Fernandes" />
+		</>
+	);
+}
+
+Meta.defaultProps = {
+	description: 'Bring me thine ideas and passion and I shall turn them into code',
+	keywords: ['home', 'full stack developer', 'full stack', 'developer', 'web', 'android', 'ios', 'flutter']
+}
