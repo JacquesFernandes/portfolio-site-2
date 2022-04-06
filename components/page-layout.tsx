447b1a7e502f98d841d7b16898155febb8bb0f81
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2022-04-06 23:11:38 +0100

    Create PageLayout Component for reuse

---
+++ components/page-layout.tsx
@@ -0,0 +1,34 @@
+import {FC} from "react";
+import Head from "next/head";
+import {Meta, MetaProps} from "./meta";
+
+export interface PageLayoutProps extends MetaProps {
+	rowLayout?: boolean;
+}
+
+export const PageLayout: FC<PageLayoutProps> = (props) => {
+  return(
+		<>
+			<>
+				<Head>
+					<title>{props.title}</title>
+					<Meta {...props} />
+				</Head>
+
+				<main className="min-h-screen p-3 bg-gradient-to-br from-gray-700 to-gray-900 text-white" >
+					<nav className="mb-4 max-w-screen-lg mx-auto" >
+						<h1 className="text-4xl font-bold" >{props.title}</h1>
+					</nav>
+
+					<article className={`${props.rowLayout? 'flex flex-row gap-4 flex-wrap' : 'space-y-4'}  max-w-screen-lg mx-auto`} >
+						{ props.children }
+					</article>
+				</main>
+			</>
+		</>
+	);
+}
+
+PageLayout.defaultProps = {
+	rowLayout: false,
+}
