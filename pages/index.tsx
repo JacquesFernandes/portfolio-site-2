e00fa9d20a54f90b21523cffbb26d76c04af5e92
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2022-04-11 02:42:23 +0100

    Fix responsiveness issue on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import {PageLayout} from "../components/page-layout";
 const Home: NextPage = () => {
   return (
     <PageLayout rowLayout title="Home" >
-      <section className="relative w-full aspect-square group max-w-xs" >
+      <section className="relative w-full aspect-square group max-w-xs mx-auto" >
         <Image src={profilePic} layout="fill" className="absolute" placeholder="blur" alt="My profile picture" />
         <div className="absolute z-10 top-0 left-0 h-full w-full p-4 flex flex-col justify-center items-center bg-gray-900 bg-opacity-80 group-hover:bg-opacity-0" >
           <p className="text-2xl transition-opacity opacity-100 group-hover:opacity-0 mb-1" >Bring me thine ideas and passion and I shall turn them into code</p>
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
         <p>Perhaps an app to go along with it for both Android and iOS?</p>
         <p className="mb-6" >Who knows - maybe a robot that runs around telling people about this stuff!</p>
         <Link href={"/build"}>
-          <a className="p-3 flex flex-row flex-nowrap justify-between max-w-xs items-center cursor-pointer bg-white text-gray-900 transition-all hover:-skew-x-6" >
+          <a className="p-3 flex flex-row flex-nowrap justify-between max-w-md items-center cursor-pointer bg-white text-gray-900 transition-all hover:-skew-x-6" >
             <span className="mr-4" >
               Say no more
             </span>
