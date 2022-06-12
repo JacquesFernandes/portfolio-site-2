b1bef81466110b44078c7c23554cd64bd48c0997
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2022-06-12 17:14:50 +0100

    Add Nav

---
+++ components/nav.tsx
@@ -0,0 +1,54 @@
+import {FC} from "react";
+import NextLink from 'next/link';
+import {useRouter} from "next/router";
+
+interface NavEntryProps {
+	href: string;
+	label: string;
+	current?: boolean;
+}
+const NavEntry: FC<NavEntryProps> = ({ href, label, current }) => {
+	if (current) {
+		return(
+			<span className="text-gray-400" >
+				<span className="hidden" >Current page: </span> {label}
+			</span>
+		);
+	}
+
+	return(
+		<NextLink href={href}>
+			<a className="hover:underline underline-offset-1" >
+				{label}
+			</a>
+		</NextLink>
+	);
+}
+
+NavEntry.defaultProps = {
+	current: false,
+}
+
+
+type PageListType = Pick<NavEntryProps, 'href' | 'label'>[]
+const pages: PageListType = [
+	{ href: "/", label: "Home" },
+	{ href: "/about",  label: "About" },
+	{ href: "/personal",  label: "Personal" },
+	{ href: "/professional",  label: "Professional" }
+]
+export const Nav: FC = () => {
+	const router = useRouter();
+
+	return(
+		<nav>
+			<ul className="flex flex-row flex-wrap" >
+				{
+					pages.map(({ label, href }, index) => <li key={`route_${index}_${label}`} className="mr-2 mb-2" >
+						<NavEntry href={href} label={label} current={href === router.pathname} />
+					</li>)
+				}
+			</ul>
+		</nav>
+	);
+}
--- components/page-layout.tsx
+++ components/page-layout.tsx
@@ -1,6 +1,7 @@
 import {FC, PropsWithChildren} from "react";
 import Head from "next/head";
 import {Meta, MetaProps} from "./meta";
+import {Nav} from "./nav";
 
 export interface PageLayoutProps extends MetaProps {
 	rowLayout?: boolean;
@@ -16,9 +17,10 @@ export const PageLayout: FC<PropsWithChildren<PageLayoutProps>> = (props) => {
 				</Head>
 
 				<main className="min-h-screen p-3 bg-gradient-to-br from-gray-700 to-gray-900 text-white" >
-					<nav className="mb-4 max-w-screen-lg mx-auto" >
+					<div className="mb-4 max-w-screen-lg mx-auto" >
 						<h1 className="text-4xl font-bold" >{props.title}</h1>
-					</nav>
+						<Nav />
+					</div>
 
 					<article className={`${props.rowLayout? 'flex flex-row gap-4 flex-wrap' : 'space-y-4'}  max-w-screen-lg mx-auto`} >
 						{ props.children }
