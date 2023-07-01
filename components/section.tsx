a197cbd17407bcda3f662ae0c599b4b5c9ee2128
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2023-07-01 21:54:56 +0100

    Update Section to accept className

--- components/section.tsx
+++ components/section.tsx
@@ -1,11 +1,12 @@
-import {FC, ReactNode} from "react";
+import {FC, HTMLAttributes, ReactNode} from "react";
 import cx from 'clsx';
 
 type SectionProps = {
 	children: ReactNode;
 	size?: 'page' | 'fit';
+	className?: HTMLAttributes<HTMLElement>['className']
 }
 
-export const Section: FC<SectionProps> = ({ children, size='page' }) => <section className={cx("h-screen w-full flex flex-col overflow-x-hidden",{ 'h-fit': size === 'fit', 'h-screen': size === 'page' || size !== 'fit' } )} >
+export const Section: FC<SectionProps> = ({ children, size='page', className }) => <section className={cx("h-screen w-full flex flex-col overflow-x-hidden", { 'h-fit': size === 'fit', 'h-screen': size === 'page' || size !== 'fit' }, className )} >
 	{children}
 </section>
