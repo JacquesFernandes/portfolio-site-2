c5642fd697a4a2ac3a17a9a32698f5a66e735747
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2023-07-01 21:55:32 +0100

    Create brief page

---
+++ pages/brief.tsx
@@ -0,0 +1,14 @@
+import {NextPage} from "next";
+import {PageLayout} from "../components/page-layout";
+import {Section} from "../components/section";
+import {Nav} from "../components/nav";
+
+const BriefPage: NextPage = () => <>
+	<PageLayout title="Brief" >
+		<Section>
+		</Section>
+		<Nav />
+	</PageLayout>
+</>;
+
+export default BriefPage;
