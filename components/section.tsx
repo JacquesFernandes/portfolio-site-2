4784f0eac99b3a24521d207e3841624e62f52596
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2023-07-16 17:41:14 +0100

    Add `padded` and `href` props to Section component

--- components/section.tsx
+++ components/section.tsx
@@ -4,9 +4,11 @@ import cx from 'clsx';
 type SectionProps = {
 	children: ReactNode;
 	size?: 'page' | 'fit';
-	className?: HTMLAttributes<HTMLElement>['className']
+	className?: HTMLAttributes<HTMLElement>['className'];
+	padded?: boolean;
+	href?: string;
 }
 
-export const Section: FC<SectionProps> = ({ children, size='page', className }) => <section className={cx("h-screen w-full flex flex-col overflow-x-hidden", { 'h-fit': size === 'fit', 'h-screen': size === 'page' || size !== 'fit' }, className )} >
+export const Section: FC<SectionProps> = ({ children, size='page', padded=true, className, href }) => <section id={href} className={cx("h-screen w-full overflow-x-hidden", { 'h-fit': size === 'fit', 'h-screen': size === 'page' || size !== 'fit', 'p-3': padded }, className )} >
 	{children}
 </section>
