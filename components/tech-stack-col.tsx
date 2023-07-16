b55f8d54530cd5ec96ca70bba364f6ad4cfd35ae
Jacques "Lawrenz" Fernandes <jacques.fernandes.1337@gmail.com>
2023-07-16 22:41:48 +0100

    Create TechStackCol component for homepage

---
+++ components/tech-stack-col.tsx
@@ -0,0 +1,45 @@
+import {FC} from "react";
+import Link, {LinkProps} from "next/link";
+
+type Tech = {
+	name: string;
+	link: LinkProps['href']
+}
+
+type TechStackCategory = {
+	name: string;
+	tech: Tech[]
+}
+
+type TechStackColProps = {
+	title: string;
+	categories: TechStackCategory[]
+}
+
+export const TechStackCol: FC<TechStackColProps> = ({ title, categories }) => <>
+	<div className=" space-y-1.5" >
+		<h3 className="text-xl font-bold" >{title}</h3>
+
+		{
+			categories.map((category, techCategoryIndex) => (
+				<details className="p-1.5 sm:p-1 bg-white text-slate-700" key={`tech_category_${techCategoryIndex}`} >
+					<summary>{category.name}</summary>
+					<ul>
+						{
+							category.tech.map((techItem, techItemIndex) => (
+								<li key={`tech_category_item_${techItemIndex}`} >
+									<Link
+										href={techItem.link}
+										className="underline"
+									>
+										{techItem.name}
+									</Link>
+								</li>
+							))
+						}
+					</ul>
+				</details>
+			))
+		}
+	</div>
+</>;
