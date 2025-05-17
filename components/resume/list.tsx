b01f6c6aa597cd50ed910a7e6cd3425723e6ebc0
Jacques Fernandes <jacques.fernandes.1337@gmail.com>
2025-05-17 02:37:26 +0100

    chore: re-init with a fresh install of nextjs @ 15

--- components/resume/list.tsx
+++
@@ -1,9 +0,0 @@
-import {FC, ReactNode} from "react";
-
-type ListProps = {
-	children: ReactNode;
-}
-
-export const List: FC<ListProps> = ({ children }) => <ul className="list-disc list-outside ml-5" >
-	{children}
-</ul>
--- constants/pages.ts
+++
@@ -1,8 +0,0 @@
-export type Pages = Record<string, `/${string}`>;
-
-export const pages: Pages = {
-	Home: "/",
-	Brief: "/brief",
-	Tech: "/tech",
-	Projects: "/projects"
-}
