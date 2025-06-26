286574d6200c02ad85601a6534aa0d9555984509
Jacques Fernandes <jacques.fernandes.1337@gmail.com>
2025-06-26 15:27:27 +0100

    Update Home page with "site under construction message"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
-  return <main className="w-screen min-h-screen p-3 md:py-0 bg-slate-700 text-neutral-100" >
-    <section className="w-full min-h-screen md:flex md:justify-center md:items-center" >
-      <h1 className="text-5xl" >Full Stack <s>Web</s> Developer</h1>
+  return <main className="w-screen min-h-screen p-3 md:py-0 bg-slate-700 text-neutral-100 flex flex-col justify-center items-center" >
+    <section className="mb-5" >
+      <h1 className="text-4xl text-center" >Full Stack <s>Web</s> Developer</h1>
     </section>
 
+    <section className="w-full bg-yellow-200 text-black -skew-x-12 border-black border-2" >
+      <h2 className="text-2xl text-center" >Site Under Construction</h2>
+    </section>
   </main>
 }
