d0541deec68b5e466dce7f222e48c88a7bb19e26
Jacques Fernandes <jacques.fernandes.1337@gmail.com>
2025-11-04 21:16:51 +0000

    fix: Remove console log in circular-array-get

--- lib/utils/circular-array-get.ts
+++ lib/utils/circular-array-get.ts
@@ -2,7 +2,6 @@
 export const circularArrayGet = <S extends unknown[]>(source: S, index: number): S[number] => {
    const possiblyNegativeCircularIndex = index % source.length;
    const circularIndex = possiblyNegativeCircularIndex < 0 ? possiblyNegativeCircularIndex + source.length : possiblyNegativeCircularIndex;
-   console.log("circularArrayGet", circularIndex);
 
    return source[circularIndex];
 }
