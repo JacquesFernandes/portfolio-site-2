5cabdcfbdae032996586f9a064a02654eece92e3
Jacques Fernandes <jacques.fernandes.1337@gmail.com>
2025-11-04 20:49:26 +0000

    feat: Major improvements to responsiveness on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import {AspectRoller} from "@/lib/components/aspect-roller";
 
 export default function Home() {
   return <main className="w-full min-h-screen bg-sky-700 flex justify-center items-center" >
-     <h1 className="text-5xl flex flex-row items-center gap-3" >
-        <span>Jacques Fernandes</span>
-        <span>|</span>
+     <h1 className="text-3xl sm:text-4xl md:text-5xl flex flex-col md:flex-row items-center gap-6 md:gap-4" >
+        <span className="text-right" >Jacques Fernandes</span>
+        <hr className="w-1/2 md:w-[1px] md:h-28 border-2 border-inherit" />
         <AspectRoller
            aspects={[
               'Front-end Engineer',
--- lib/components/aspect-roller.tsx
+++ lib/components/aspect-roller.tsx
@@ -23,16 +23,14 @@ export const AspectRoller: FC<AspectRollerProps> = ({ aspects }) => {
       circularArrayGet(aspects, focusIndex+1),
    ];
 
-   console.log('circular:', focusIndex-1, focusIndex, focusIndex+1, aspects, displayAspects)
-
-   return <div className="min-w-md flex flex-col items-stretch gap-2" >
+   return <div className="min-w-72 md:min-w-md flex flex-col items-stretch gap-2" >
       <button
-         className="w-full transition-all duration-150 bg-transparent border border-transparent hover:border-inherit rounded-xl flex justify-center items-center cursor-pointer"
+         className="w-full transition-all duration-150 bg-transparent border border-transparent hover:border-inherit rounded-xl hidden md:flex justify-center items-center cursor-pointer"
          onClick={() => setFocusIndex((i) => i-1)}
       >
          <ChevronUp size="2.5rem" />
       </button>
-      <ul>
+      <ul className="text-center md:text-left" >
          <li className="opacity-80" >{displayAspects[0]}</li>
          <li className="opacity-100" >{displayAspects[1]}</li>
          <li className="opacity-80" >{displayAspects[2]}</li>
